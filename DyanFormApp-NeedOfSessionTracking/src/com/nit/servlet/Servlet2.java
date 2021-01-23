6ea7621a9e30e6a2d2a177c1814b99563391d32c
vikash kumar gupta <vikash kumar gupta@LAPTOP-PM40QTLP>
2021-01-23 22:24:18 +0530

    SessionTracking1

---
+++ DyanFormApp-NeedOfSessionTracking/src/com/nit/servlet/Servlet2.java
@@ -0,0 +1,50 @@
+package com.nit.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/secondurl")
+public class Servlet2 extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("Servlet2.doGet()");
+
+		//		set ContentType
+		res.setContentType("text/html");
+
+		//		creating PrintWriter object
+		PrintWriter pw = res.getWriter();
+
+		//		reading form1 data
+		String name = req.getParameter("name");
+		String fname = req.getParameter("fname");
+		String ms = req.getParameter("ms");
+
+		//		reading form2 data
+
+		String f2t1 = req.getParameter("f2t1");
+		String f2t2 = req.getParameter("f2t2");
+
+		//		printing form1 data and form2 data
+		pw.print("Name::" + name + "....Father Name::" + fname + "....Marital Status::" + ms);
+
+		pw.print("f2v1::" + f2t1 + "....f2v2::" + f2t2);
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("Servlet2.doPost()");
+		doGet(req, res);
+	}
+
+}
